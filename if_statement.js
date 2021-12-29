b47c4701a3cae72a3ae8bb36cfa81adc1b0efae3
Soma Roy <68267878+somaroy29@users.noreply.github.com>
2021-12-30 00:38:40 +0530

    Add files via upload

---
+++ if_statement.js
@@ -0,0 +1,27 @@
+var yournamesoma = true;
+//first method
+if (yournamesoma) {
+  console.log("register her name");
+}
+//second method
+if (true) {
+  console.log("register her name");
+}
+//third method
+if (yournamesoma == false) {
+  console.log("do not register her name");
+}
+//another example
+if (7 > 5) {
+  console.log("true");
+}
+//if..else
+if (7 < 4) {
+  console.log("true");
+}
+else {
+    console.log("false")
+}
+
+
+  
---
+++ ifelse.js
@@ -0,0 +1,12 @@
+//if..else condition
+let myage=25;
+if (myage > 30) {
+    console.log("false");
+}
+else if (myage==20){
+      console.log("false")
+  }
+else{
+    console.log("yes my age")
+}
+  
