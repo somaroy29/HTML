0005afa5aa6e9fd30f0672b3f0e3e0e706284c75
Soma Roy <68267878+somaroy29@users.noreply.github.com>
2022-01-05 16:25:58 +0530

    Add files via upload

---
+++ while.js
@@ -0,0 +1,14 @@
+//example-1
+let a = 5; //initialization
+while (a < 10) { // Condition 
+  console.log("print a"); //printing
+  a++; //increment
+}
+
+
+//example-2
+let ab=5;let i=0;
+while(i<ab){
+    console.log("number is",i)
+    i++;
+}
